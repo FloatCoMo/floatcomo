bb47b939b8b905a8364175487c7d54dab703426b
Connor Knabe <ConnorKnabe@gmail.com>
2016-02-27 13:37:11 -0600

    Adding custom js to hide the navbar

---
+++ _site/flafy/js/custom.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+    $(".navbar-nav li a").click(function(event) {
+        if (!$(this).parent().hasClass('dropdown'))
+            $(".navbar-collapse").collapse('hide');
+    });
+});
