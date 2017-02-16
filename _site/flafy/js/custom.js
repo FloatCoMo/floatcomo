a4dbae8b52511f2b07b41272e971847c3d45d600
Connor Knabe <ConnorKnabe@gmail.com>
2017-02-16 14:26:12 -0600

    Not using the loader

--- _site/flafy/js/custom.js
+++ _site/flafy/js/custom.js
@@ -6,4 +6,18 @@ $(document).ready(function(){
     });
 
 
+
+   $(function () {
+        if ($("[data-lazy-load-image]").length > 0) {
+            $("[data-lazy-load-image]").each(function (index, element) {
+                var img = new Image();
+                img.src = $(element).data("lazy-load-image");
+                if (typeof $(element).data("image-classname" !== "undefined"))
+                    img.className = $(element).data("image-classname");
+                $(element).append(img);
+            });
+        }
+    });
+
+
 });
