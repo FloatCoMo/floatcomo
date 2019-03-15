a625969467f8cfd1c5bde61d268491dfafe7ef6c
Connor Knabe <ConnorKnabe@gmail.com>
2019-03-14 20:28:39 -0500

    using smaller pod photo

--- flafy/js/custom.js
+++ flafy/js/custom.js
@@ -1,23 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function() {
+	$('.navbar-nav li a').click(function(event) {
+		if (!$(this).parent().hasClass('dropdown')) $('.navbar-ex1-collapse').collapse('hide');
+	});
 
-    $(".navbar-nav li a").click(function(event) {
-        if (!$(this).parent().hasClass('dropdown'))
-            $(".navbar-ex1-collapse").collapse('hide');
-    });
-
-
-
-   $(function () {
-        if ($("[data-lazy-load-image]").length > 0) {
-            $("[data-lazy-load-image]").each(function (index, element) {
-                var img = new Image();
-                img.src = $(element).data("lazy-load-image");
-                if (typeof $(element).data("image-classname" !== "undefined"))
-                    img.className = $(element).data("image-classname");
-                $(element).append(img);
-            });
-        }
-    });
-
-
+	$(function() {
+		if ($('[data-lazy-load-image]').length > 0) {
+			$('[data-lazy-load-image]').each(function(index, element) {
+				var img = new Image();
+				img.src = $(element).data('lazy-load-image');
+				if (typeof $(element).data('image-classname' !== 'undefined')) img.className = $(element).data('image-classname');
+				$(element).append(img);
+			});
+		}
+	});
 });
