682b699b324fb690d75ad22a2ab43e4bb8118d0a
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2021-11-23 09:20:19 -0600

    adding black friday deals

--- flafy/js/custom.js
+++ flafy/js/custom.js
@@ -1,26 +1,34 @@
-$(document).ready(function() {
-	$('.navbar-nav li a').click(function(event) {
+$(document).ready(function () {
+	$('.navbar-nav li a').click(function (event) {
 		if (!$(this).parent().hasClass('dropdown')) $('.navbar-ex1-collapse').collapse('hide');
 	});
 
+	$('#holiday-deal').modal();
+	$(function () {
+		$('#holiday-deal').on('click', function () {
+			$($(this).data('modal')).modal();
+			return false;
+		});
+	});
 
-	var newYear = new Date("Janurary 1, 2021");
+	var newYear = new Date("November 29, 2022");
 	var now = new Date();
-	if (now < newYear){
-			$('#holiday-deal').modal({
-				escapeClose: false,
-				clickClose: false,
-				showClose: false
-			});
+	if (now < newYear) {
+		$('#holiday-deal').modal({
+			escapeClose: false,
+			clickClose: false,
+			showClose: false
+		});
 	} else {
+		console.log("HIDE BOGO");
 		$(".BOGO").hide();
 	}
 
-	
 
-	$(function() {
+
+	$(function () {
 		if ($('[data-lazy-load-image]').length > 0) {
-			$('[data-lazy-load-image]').each(function(index, element) {
+			$('[data-lazy-load-image]').each(function (index, element) {
 				var img = new Image();
 				img.src = $(element).data('lazy-load-image');
 				if (typeof $(element).data('image-classname' !== 'undefined')) img.className = $(element).data('image-classname');
@@ -30,6 +38,6 @@ $(document).ready(function() {
 	});
 });
 
-function closeModal(){
+function closeModal() {
 	$.modal.close();
 }
